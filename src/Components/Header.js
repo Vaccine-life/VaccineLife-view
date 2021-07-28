3d289d6f4935b0f96ef532ab8549eeef5037e846
wglee0511 <ranazang@naver.com>
2021-07-28 15:42:11 +0900

    moment

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,12 @@
+import moment from "moment";
 import React from "react";
 
 const Header = () => {
-  return <div>Header 다은짱 효유니도짱</div>
+  return (
+    <div>
+      Header 다은짱 효유니도짱 {moment(new Date()).format("YYYY-MM-DD")}
+    </div>
+  );
 };
 
 export default Header;
