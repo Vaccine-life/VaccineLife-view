75235b9e33fbca76f4d4fb97bb055849f0c77394
wglee0511 <ranazang@naver.com>
2021-07-29 10:11:35 +0900

    Grid Button

--- src/pages/Write.js
+++ src/pages/Write.js
@@ -1,6 +1,13 @@
 import React from "react";
+import logger from "../shared/logger";
+import { history } from "../redux/configStore";
 
 const Write = () => {
+  const url = history.location.pathname;
+  // /vboard/write일때 true /qboard/write 일떄 false
+  const urlExchanger = url === "/vboard/write" ? true : false;
+
+  logger(urlExchanger);
   return <div>write</div>;
 };
 
