ecca0f176c630c9d954d53e6a3fda34443aaf97f
rectanglehihat <80513184+rectanglehihat@users.noreply.github.com>
2021-07-30 09:06:02 +0900

    효윤-header, banner, footer

--- src/pages/Medical.js
+++ src/pages/Medical.js
@@ -1,16 +1,26 @@
 import React from "react";
+import { Text, Grid } from "../elements";
 import Comment from "../components/Comment";
+import styled from "styled-components";
+
 
 const Medical = () => {
 
   return (
     <React.Fragment>
-      <div>Medical</div>
-      <textarea placeholder="응원의 한마디!"/>
-      <button>등록</button>
-      <Comment/>
+      <Wrapper>
+        <Grid align="left" padding="20px 0 0 0">
+          <Text bold size="24px" lineHeight="2">의료진분들께</Text>
+        </Grid>
+      
+        <Comment/>
+      </Wrapper>
     </React.Fragment>
   );
 };
+
+const Wrapper = styled.div`
+  padding: 0 5rem;
+`;
 
 export default Medical;
