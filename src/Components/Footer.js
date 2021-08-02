36dbd2c8bc7df3d875ed5dd7ec2fda0b5dd7772e
ElaineLee21 <playder1427@gmail.com>
2021-08-02 19:45:23 +0900

    push before pull

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,29 +9,51 @@ const Footer = () => {
   return (
     <React.Fragment>
       <FixedFooter>
-
-        <div style={{backgroundColor:"#242424", display:"flex", alignItems:"center"}}>
+        <div
+          style={{
+            backgroundColor: "#242424",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
           <Grid margin="2rem 2rem 2rem 5rem" width="auto">
-            <Text color="#ffffff" cursor="pointer">슬기로운</Text>
-            <Text color="#ffffff" cursor="pointer" size="16" bold>백신생활</Text>
+            <Text color="#ffffff" cursor="pointer">
+              슬기로운
+            </Text>
+            <Text color="#ffffff" cursor="pointer" size="16" bold>
+              백신생활
+            </Text>
           </Grid>
 
           <Grid margin="2rem 0" width="0 auto">
-            <ul style={{display:"flex"}}>
-              <Text color="#ffffff" size="12px" margin="5px">백신후기</Text>
-              <Text color="#ffffff" size="12px" margin="5px">의료진분들께 한마디</Text>
+            <ul style={{ display: "flex" }}>
+              <Text color="#ffffff" size="12px" margin="5px">
+                백신후기
+              </Text>
+              <Text color="#ffffff" size="12px" margin="5px">
+                의료진분들께 한마디
+              </Text>
             </ul>
 
-            <ul style={{display:"flex"}}>
-              <Text color="#A5A5A5" size="12px" margin="5px">대표자명</Text>
-              <Text color="#A5A5A5" size="12px" margin="5px">|</Text>
-              <Text color="#A5A5A5" size="12px" margin="5px">Contact. playder1427@gmail.com</Text>
-              <Text color="#A5A5A5" size="12px" margin="5px">|</Text>
-              <Text color="#A5A5A5" size="12px" margin="5px">Github. https://github.com/Vaccine-life</Text>
+            <ul style={{ display: "flex" }}>
+              <Text color="#A5A5A5" size="12px" margin="5px">
+                대표자명
+              </Text>
+              <Text color="#A5A5A5" size="12px" margin="5px">
+                |
+              </Text>
+              <Text color="#A5A5A5" size="12px" margin="5px">
+                Contact. playder1427@gmail.com
+              </Text>
+              <Text color="#A5A5A5" size="12px" margin="5px">
+                |
+              </Text>
+              <Text color="#A5A5A5" size="12px" margin="5px">
+                Github. https://github.com/Vaccine-life
+              </Text>
             </ul>
           </Grid>
         </div>
-
       </FixedFooter>
     </React.Fragment>
   );
