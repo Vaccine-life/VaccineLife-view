ae8cfa15f5470216f37182c57df80097efa73b7e
wglee0511 <ranazang@naver.com>
2021-08-12 21:25:31 +0900

    detail

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,13 +3,13 @@ import { useSelector } from "react-redux";
 import styled from "styled-components";
 import Login from "./Login";
 import Alert from "../components/popup/Alert";
-import IntroCharacter from "../images/IntroCharacter.png"
+import IntroCharacter from "../images/IntroCharacter.png";
 
 import Map from "../components/Map";
 import theme from "../styles/theme";
 import MainTo from "../components/MainTo";
 import MainPopular from "../components/MainPopular";
-import MainNivoBar from '../components/MainNivoBar';
+import MainNivoBar from "../components/MainNivoBar";
 
 const Main = () => {
   // Main페이지에서도 로그인모달창이 뜨게 함
@@ -43,45 +43,44 @@ const Main = () => {
 };
 
 const Intro = styled.div`
-/* display: flex;
+  /* display: flex;
 position: relative; */
-display: flex;
-flex-direction: row;
-justify-content: center;
-align-items: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
 
-width: 100vw;
-height: 230px;
-background-color: ${theme.bg};
+  width: 100vw;
+  height: 230px;
+  background-color: ${theme.bg};
 
-& > img {
-width: auto;
-height: auto;
-max-width: 330px;
-max-height: 330px;
+  & > img {
+    width: auto;
+    height: auto;
+    max-width: 330px;
+    max-height: 330px;
 
-padding-top: 92px;
+    padding-top: 92px;
+  }
 
-}
+  & > h1 {
+    padding-left: 0px;
+    padding-top: 75px;
+    font-size: ${theme.headTwoHeight};
+    line-height: 42px;
 
-& > h1 {
-padding-left: 0px;
-padding-top: 75px;
-font-size: ${theme.headTwoHeight};
-line-height: 42px;
+    color: #ffffff;
 
-color: #ffffff;
-
-& > span {
-font-weight: 600;
-}
-}
+    & > span {
+      font-weight: 600;
+    }
+  }
 `;
 
 const MapAndChart = styled.div`
-display: flex;
-justify-content: center;
-margin: 80px auto 60px auto;
+  display: flex;
+  justify-content: center;
+  margin: 80px auto 60px auto;
 `;
 
 export default Main;
