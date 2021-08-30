4372d25ce3b7f4bbed85f422a6fb95c66637f125
rectanglehihat <80513184+rectanglehihat@users.noreply.github.com>
2021-08-30 23:12:34 +0900

    hyoni - navModal 주석

--- src/Components/mobile/NavModal.js
+++ src/Components/mobile/NavModal.js
@@ -4,24 +4,29 @@ import theme from "../../styles/theme";
 import { Grid, Image, Text } from "../../elements";
 import { useDispatch, useSelector } from "react-redux";
 import { actionVisible, actionNavVisible } from "../../redux/modules/modal";
-import { actionLogoutCookie } from "../../redux/modules/user";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import { history } from "../../redux/configStore";
 import { withRouter } from "react-router-dom";
 import login from "../../images/login.png";
-import logout from "../../images/logout.png";
 import user from "../../images/user.png";
 import Arrow from "../../images/Arrow.png";
 
+// 모바일 버전 헤더에서 faBars아이콘 클릭시 뜨는 모달.
+// 데스크탑 버전의 헤더를 모바일 버전으로 변경하기 위해 생성.
 const NavModal = (props) => {
   const dispatch = useDispatch();
 
+  // 로그인여부를 리덕스에서 가져와 로그인과 로그아웃 헤더를 분기함.
   const is_login = useSelector((state) => state.user.is_login);
+  // 닉네임을 리덕스에서 가져와 로그인시 유저닉네임 보여줌.
   const nickname = useSelector((state) => state.user.user.nickname);
+  // 현재 페이지의 경로명(pathname)을 가져옴.
+  // 이 컴포넌트를 withRouter로 감싸서 history사용할 수 있게 함.
   const url = history.location.pathname;
 
   // 모달 바깥 부분 클릭시 모달 off
+  // 모달을 감싼 부분에 onClick으로 적용
   const handleModalOff = (e) => {
     const clicked = e.target.closest(".modal");
     // console.log(clicked);
@@ -32,6 +37,7 @@ const NavModal = (props) => {
     }
   };
 
+  // 로그인 상태의 헤더모달
   if (is_login) {
     return (
       <>
@@ -83,6 +89,7 @@ const NavModal = (props) => {
                   }}
                 />
               </TextIconWrapper>
+              {/* 회색 경계선 */}
               <div
                 style={{ borderBottom: "1px solid #F7F7F7", margin: "2rem 0" }}
               />
@@ -135,36 +142,13 @@ const NavModal = (props) => {
               >
                 의료진분들께
               </Text>
-              {/* <div
-                style={{ borderBottom: "1px solid #F7F7F7", margin: "2rem 0" }}
-              /> */}
-
-              {/* <TextIconWrapper
-                onClick={() => {
-                  dispatch(actionNavVisible());
-                  dispatch(actionLogoutCookie());
-                }}
-              >
-                <Text
-                  color={theme.typoBlack}
-                  size={theme.bodyThreeSize}
-                  margin="2rem 0"
-                >
-                  로그아웃
-                </Text>
-                <Image
-                  shape="square"
-                  size="1.5rem"
-                  margin="0 0 0 3px"
-                  src={logout}
-                />
-              </TextIconWrapper> */}
             </Grid>
           </Modal>
         </Wrapper>
       </>
     );
   }
+  // 로그아웃 상태의 헤더모달
   return (
     <>
       <Wrapper
@@ -267,7 +251,7 @@ const Wrapper = styled.div`
   height: 100%;
   position: fixed;
   top: 0;
-  left: 0;
+  /* left: 0; */
   z-index: 2;
   background-color: rgba(0, 0, 0, 0.5);
 `;
@@ -275,15 +259,15 @@ const Wrapper = styled.div`
 const Modal = styled.div`
   width: 50%;
   height: 90%;
+  /* 오른쪽에 붙게 정렬 */
   position: relative;
-  /* top: 50%;
-  transform: translateY(-50%); */
   left: 30%;
+
   background-color: white;
   display: flex;
   flex-direction: column;
-  justify-content: right;
-  align-items: right;
+  /* justify-content: right; */
+  /* align-items: right; */
   padding: 10%;
 `;
 
